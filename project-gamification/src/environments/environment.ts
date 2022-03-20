67da971951d9a2c9b249b70b35d3d7e5b7a7470c
Jenny980 <jennytangarife98@gmail.com>
2022-03-20 14:07:14 -0500

    Conexión con firebase y modificaciones iniciales en la vista

--- project-gamification/src/environments/environment.ts
+++ project-gamification/src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBQwkmmDoQz9-5KjCHxk99JTCoOXHOOaxo',
+    authDomain: 'gamification-86cd7.firebaseapp.com',
+    databaseURL: 'https://gamification-86cd7-default-rtdb.firebaseio.com',
+    projectId: 'gamification-86cd7',
+    storageBucket: 'gamification-86cd7.appspot.com',
+    messagingSenderId: '812300645850',
+    appId: '1:812300645850:web:9e30ec28dfa05417ae3ce3',
+    measurementId: 'G-NYTKF4NXYX',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
